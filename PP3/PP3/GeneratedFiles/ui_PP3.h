50468cd0f5089b57fd2026eeea8a5abaaae37dcc
lucasmoison <46816998+lucasmoison@users.noreply.github.com>
2019-01-24 16:52:10 +0100

    Feature/reservation (#6)
    * init project
    
    * init project
    
    * Delete test.html

---
+++ PP3/PP3/GeneratedFiles/ui_PP3.h
@@ -0,0 +1,82 @@
+/********************************************************************************
+** Form generated from reading UI file 'PP3.ui'
+**
+** Created by: Qt User Interface Compiler version 5.11.2
+**
+** WARNING! All changes made in this file will be lost when recompiling UI file!
+********************************************************************************/
+
+#ifndef UI_PP3_H
+#define UI_PP3_H
+
+#include <QtCore/QVariant>
+#include <QtWidgets/QApplication>
+#include <QtWidgets/QMainWindow>
+#include <QtWidgets/QMenuBar>
+#include <QtWidgets/QStatusBar>
+#include <QtWidgets/QTabWidget>
+#include <QtWidgets/QToolBar>
+#include <QtWidgets/QWidget>
+
+QT_BEGIN_NAMESPACE
+
+class Ui_PP3Class
+{
+public:
+    QWidget *centralWidget;
+    QTabWidget *tabWidget;
+    QWidget *tab;
+    QWidget *tab_2;
+    QMenuBar *menuBar;
+    QToolBar *mainToolBar;
+    QStatusBar *statusBar;
+
+    void setupUi(QMainWindow *PP3Class)
+    {
+        if (PP3Class->objectName().isEmpty())
+            PP3Class->setObjectName(QStringLiteral("PP3Class"));
+        PP3Class->resize(881, 759);
+        centralWidget = new QWidget(PP3Class);
+        centralWidget->setObjectName(QStringLiteral("centralWidget"));
+        tabWidget = new QTabWidget(centralWidget);
+        tabWidget->setObjectName(QStringLiteral("tabWidget"));
+        tabWidget->setGeometry(QRect(24, 19, 821, 661));
+        tab = new QWidget();
+        tab->setObjectName(QStringLiteral("tab"));
+        tabWidget->addTab(tab, QString());
+        tab_2 = new QWidget();
+        tab_2->setObjectName(QStringLiteral("tab_2"));
+        tabWidget->addTab(tab_2, QString());
+        PP3Class->setCentralWidget(centralWidget);
+        menuBar = new QMenuBar(PP3Class);
+        menuBar->setObjectName(QStringLiteral("menuBar"));
+        menuBar->setGeometry(QRect(0, 0, 881, 26));
+        PP3Class->setMenuBar(menuBar);
+        mainToolBar = new QToolBar(PP3Class);
+        mainToolBar->setObjectName(QStringLiteral("mainToolBar"));
+        PP3Class->addToolBar(Qt::TopToolBarArea, mainToolBar);
+        statusBar = new QStatusBar(PP3Class);
+        statusBar->setObjectName(QStringLiteral("statusBar"));
+        PP3Class->setStatusBar(statusBar);
+
+        retranslateUi(PP3Class);
+
+        QMetaObject::connectSlotsByName(PP3Class);
+    } // setupUi
+
+    void retranslateUi(QMainWindow *PP3Class)
+    {
+        PP3Class->setWindowTitle(QApplication::translate("PP3Class", "PP3", nullptr));
+        tabWidget->setTabText(tabWidget->indexOf(tab), QApplication::translate("PP3Class", "Tab 1", nullptr));
+        tabWidget->setTabText(tabWidget->indexOf(tab_2), QApplication::translate("PP3Class", "Tab 2", nullptr));
+    } // retranslateUi
+
+};
+
+namespace Ui {
+    class PP3Class: public Ui_PP3Class {};
+} // namespace Ui
+
+QT_END_NAMESPACE
+
+#endif // UI_PP3_H
---
+++ PP3/PP3/PP3.cpp
@@ -0,0 +1,7 @@
+#include "PP3.h"
+
+PP3::PP3(QWidget *parent)
+	: QMainWindow(parent)
+{
+	ui.setupUi(this);
+}
